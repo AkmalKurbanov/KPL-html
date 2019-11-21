e4f503886c0b62c82b0d71ee66bb66f81f17330c
AkmalKurbanov <250296a@gmail.com>
2019-11-21 22:53:58 +0600

    searc form

--- src/js/scripts/scripts.js
+++ src/js/scripts/scripts.js
@@ -1,3 +1,24 @@
 $(function () {
- 
+    $('.comprising').on('click', function () {
+        $(this).parent().parent().next().slideUp();
+        $('.comprising').not(this).removeClass('sub-menu-open ul').children('ul').slideUp();
+        $(this).toggleClass('sub-menu-open').children('ul').slideToggle().next().addClass('open-icon');
+    });
+    $('.comprisingTable').on('click', function () {
+        $(this).parent().parent().next().slideToggle();
+        $('.comprising ul').slideUp();
+    });
+    $(document).mouseup(function (e) {
+        var div = $(".comprising ul");
+        if (!div.is(e.target) 
+            &&
+            div.has(e.target).length === 0) { 
+        }
+    });
+
+    $('.search-btn').on('click', function () {
+        $('.comprising').removeClass('sub-menu-open');
+        $('.search').fadeToggle();
+    });
+
 });
