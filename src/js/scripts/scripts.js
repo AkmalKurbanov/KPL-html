80ca2b9f4f265b7aad7fb6bec0a93322e05c4f33
AkmalKurbanov <250296a@gmail.com>
2019-11-22 21:12:19 +0600

    header descktop end

--- src/js/scripts/scripts.js
+++ src/js/scripts/scripts.js
@@ -1,24 +1,33 @@
 $(function () {
-    $('.comprising').on('click', function () {
-        $(this).parent().parent().next().slideUp();
-        $('.comprising').not(this).removeClass('sub-menu-open ul').children('ul').slideUp();
-        $(this).toggleClass('sub-menu-open').children('ul').slideToggle().next().addClass('open-icon');
-    });
-    $('.comprisingTable').on('click', function () {
-        $(this).parent().parent().next().slideToggle();
-        $('.comprising ul').slideUp();
-    });
-    $(document).mouseup(function (e) {
-        var div = $(".comprising ul");
-        if (!div.is(e.target) 
-            &&
-            div.has(e.target).length === 0) { 
-        }
+    $('.nav__item').children('a').on('click', function (e) {
+        event.preventDefault();
+        $(this).toggleClass('nav__link--open');
+        $(this).parent().parent().parent().next().fadeOut(300);
+        $(this).next().fadeToggle(300);
+        $('.nav__link').not(this).next().fadeOut(300);
+        $('.nav__link').not(this).removeClass('nav__link--open');
+        $('.search-btn-close').fadeOut(300);
+        $('.search-btn').fadeIn(300);
     });
 
+    $('.nav__item').on('click', function (e) {
+        $(this).find('.club-list-wrap.club').parent().css('position', 'inherit');
+    });
+    var containerWidth = $('.container').width();
+    var clubListWidth = $('.club').css('min-width', containerWidth);
+
     $('.search-btn').on('click', function () {
-        $('.comprising').removeClass('sub-menu-open');
-        $('.search').fadeToggle();
+        $(this).hide();
+        $('.search-btn-close').css('display', 'flex');
+        $('.sub-menu,.sub-block').fadeOut(300);
+        $('.search').fadeToggle(300);
+        $('.nav__link').removeClass('nav__link--open');
+    });
+
+    $('.search-btn-close').on('click', function () {
+        $(this).hide();
+        $('.search-btn').css('display', 'flex');
+        $('.search').toggle();
     });
 
 });
