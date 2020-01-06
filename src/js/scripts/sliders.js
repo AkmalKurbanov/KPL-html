5a94c0359ec372aca74475c0ce2a156ba715c102
AkmalKurbanov <250296a@gmail.com>
2020-01-06 23:49:54 +0600

    main page responsive end

--- src/js/scripts/sliders.js
+++ src/js/scripts/sliders.js
@@ -3,17 +3,32 @@ var swiper = new Swiper('.partners-js', {
   slidesPerView: 4,
   slidesPerGroup: 4,
   loop: true,
+  spaceBetween: 30,
   autoplay: {
     delay: 5000,
     disableOnInteraction: false,
   },
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+    },
+    460: {
+      slidesPerView: 3,
+    },
+    576: {
+      slidesPerView: 3,
+    },
+    992: {
+      slidesPerView: 4,
+    }
+  }
 });
 //  partners end
 
 
 // championship
 var swiper = new Swiper('.championship-js', {
-slidesPerView: 8,
+  slidesPerView: 8,
   breakpoints: {
     320: {
       slidesPerView: 2,
